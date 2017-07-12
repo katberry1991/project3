6bd269bc9347632b26fb8ee6a9533cfbc450c240
katberry1991 <kingsberrykathryn@gmail.com>
2017-07-11 19:33:17 -0700

    project

--- public/app.js
+++ public/app.js
@@ -30,35 +30,36 @@
 				authReq: false
 			})
 			.state('registration', {
-				url: '/',
-				controller: 'CaseController as vm',
-				templateUrl: 'index.html',
+				url: '/registration',
+				controller: 'RegistrationController as vm',
+				templateUrl: 'views/registration.html',
 				authReq: false
 			})
+			.state('login', {
+				url: '/login',
+				controller: 'LoginController as vm',
+				templateUrl: 'views/login.html',
+				authReq: false
+			})
+			.state('kioskCaseInfo', {
+							url: '/',
+							controller: 'KioskCaseInfoController as vm',
+							templateUrl: 'views/kioskCaseInfo.html',
+							authReq: false
+						})
+			.state('caseSummary', {
+							url: '/',
+							controller: 'caseSummaryController as vm',
+							templateUrl: 'views/caseSummary.html',
+							authReq: false
+						})
+			.state('caseinfo', {
+							url: '/',
+							controller: 'CaseInfoController as vm',
+							templateUrl: 'controllers/CaseInfoController.html',
+							authReq: false
+						})
+
 		$urlRouterProvider.otherwise('/');
 	}
-.state('login', {
-				url: '/',
-				controller: 'CaseController as vm',
-				templateUrl: 'index.html',
-				authReq: false
-			})
-.state('kioskCaseInfo', {
-				url: '/',
-				controller: 'CaseController as vm',
-				templateUrl: 'index.html',
-				authReq: false
-			})
-.state('caseSummary', {
-				url: '/',
-				controller: 'CaseController as vm',
-				templateUrl: 'index.html',
-				authReq: false
-			})
-.state('caseinfo', {
-				url: '/',
-				controller: 'CaseController as vm',
-				templateUrl: 'index.html',
-				authReq: false
-			})
 })();
